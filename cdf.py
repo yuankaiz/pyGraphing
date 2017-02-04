6c2dd28e94404efa4e881649472764184f04266b
Yuankai Zhang <yuankai@YKMBP.local>
2017-02-03 21:39:27 -0500

    adds cdf.py

---
+++ cdf.py
@@ -0,0 +1,11 @@
+import matlotlib
+
+def cdf(object):
+    def __init__(self, ln='line_name', lc='r'):
+        self.data = []
+        self.line_name = ln
+        self.line_color = lc
+
+    def loadData(self, data):
+        self.data = data
+
